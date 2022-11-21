8c93aea583c86f3ec2e7630671e15ccd1e84d464
Terdanny1 <terdanconcept68@gmail.com>
2022-11-21 02:52:16 +0100

    create the nav-bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
+    // fontFamily :{
+    //   'body': 'Sora, sans-serif'
+    // }
     extend: {},
   },
   plugins: [],
